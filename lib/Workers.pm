3dbeb5415c680a4f20c3553bf66eab4cc3f00814
Shoichi Kaji <skaji@cpan.org>
2016-07-19 05:54:05 +0900

    doc

--- lib/Workers.pm
+++ lib/Workers.pm
@@ -227,7 +227,8 @@ Workers - Blah blah blah
 
     use Workers;
 
-    my $master;
+    my $master = Your::Master->new;
+
     my $workers = Workers->new(5, sub {
         my $job = shift;
         # do work
@@ -235,19 +236,19 @@ Workers - Blah blah blah
         return $result;
     });
 
-    while (1) {
-        if (my @job = $master->get_job) {
-            my @ready = $workers->wait;
-            $master->register($_->result) for grep $_->has_result, @ready;
-            my $n = @job < @ready ? $#job : $#ready;
-            map { $ready[$_]->work($job[$_]) } 0 .. $n;
-        } else {
-            last unless my @running = $workers->is_running;
-            $workers->wait(@running);
-        }
-    }
+    # wrap Master's get_job
+    my $get_job; $get_job = sub {
+      my $self = shift;
+      if (my @job = $self->get_job) {
+        return @job;
+      }
+      return unless my @running = $workers->is_running;
+      my @ready = $workers->wait(@running);
+      $master->register($_->result) for grep $_->has_result, @ready;
+      $self->$get_job;
+    };
 
-    while (my @job = $master->get_job) {
+    while (my @job = $master->$get_job) {
         my @ready = $workers->wait;
         $master->register($_->result) if grep $_->has_result, @ready;
         my $n = @job < @ready ? $#job : $#ready;
