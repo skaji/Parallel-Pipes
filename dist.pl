f444ec99238017acfad31aecda2d89f740dd1501
Shoichi Kaji <skaji@cpan.org>
2026-05-21 23:26:35 +0900

    Raise minimum Perl to v5.24

--- dist.pl
+++ dist.pl
@@ -1,10 +1,35 @@
+use v5.42;
+
+package Trial {
+    use Moose;
+    with 'Dist::Zilla::Role::FileMunger';
+    sub munge_file ($self, $file) {
+        return if !($ENV{DZIL_RELEASING} && $file->name eq $self->zilla->main_module->name);
+        my @line;
+        for my $line (split /\n/, $file->content, -1) {
+            if ($line =~ /^our \$TRIAL/) {
+                my $trial_line = sprintf 'our $TRIAL = %d;', $self->zilla->is_trial ? 1 : 0;
+                push @line, $trial_line;
+            } else {
+                push @line, $line;
+            }
+        }
+        $file->content(join "\n", @line);
+    }
+}
+
+package NextRelease {
+    use Moose;
+    extends 'Dist::Zilla::Plugin::NextRelease';
+    sub after_release ($self, @) {} # noop
+}
+
 my @prereq = (
     [ Prereqs => 'ConfigureRequires' ] => [
-        'Module::Build::Tiny' => '0.052',
-        'perl' => '5.008001',
+        'Module::Build::Tiny' => '0.053',
     ],
     [ Prereqs => 'RuntimeRequires' ] => [
-        'perl' => '5.008001',
+        'perl' => 'v5.24',
     ],
     [ Prereqs => 'TestRequires' ] => [
         'Test::More' => '0.98',
@@ -19,32 +44,29 @@ my @no_index = (
 );
 
 my @plugin = (
-    'ExecDir' => [ dir => 'script' ],
     'Git::GatherDir' => [ exclude_filename => 'META.json' ],
-    'CopyFilesFromBuild' => [ copy => 'META.json' ],
+    'CopyFilesFromBuild' => [ copy => 'META.json', copy => 'Changes' ],
     'VersionFromMainModule' => [],
     'ReversionOnRelease' => [ prompt => 1 ],
-    'NextRelease' => [ format => '%v  %{yyyy-MM-dd HH:mm:ss VVV}d%{ (TRIAL RELEASE)}T' ],
+    '=NextRelease' => [ format => '%v  %{yyyy-MM-dd}d%{ (TRIAL RELEASE)}T' ],
+    '=Trial' => [],
     'Git::Check' => [ allow_dirty => 'Changes', allow_dirty => 'META.json' ],
     'GithubMeta' => [ issues => 1 ],
     'ReadmeAnyFromPod' => [ type => 'markdown', filename => 'README.md', location => 'root' ],
     'MetaProvides::Package' => [ inherit_version => 0, inherit_missing => 0 ],
-    'PruneFiles' => [ filename => 'dist.pl', filename => 'README.md', match => '^(xt|author|maint|example|eg)/' ],
-    'GitHubREADME::Badge' => [ badges => 'github_actions/test.yml' ],
-    'GenerateFile' => [ filename => 'Build.PL', content => "use Module::Build::Tiny;\nBuild_PL();" ],
     'MetaJSON' => [],
     'Metadata' => [ x_static_install => 1 ],
     'Git::Contributors' => [],
 
     'CheckChangesHasContent' => [],
     'ConfirmRelease' => [],
-    'UploadToCPAN' => [],
+    'FakeRelease' => [],
     'CopyFilesFromRelease' => [ match => '\.pm$' ],
-    'Git::Commit' => [ commit_msg => '%v', allow_dirty => 'Changes', allow_dirty => 'META.json', allow_dirty_match => '\.pm$' ],
-    'Git::Tag' => [ tag_format => '%v', tag_message => '%v' ],
+    'Git::Commit' => [ commit_msg => '%v%t', allow_dirty => 'Changes', allow_dirty => 'META.json', allow_dirty_match => '\.pm$' ],
+    'Git::Tag' => [ tag_format => '%v%t', tag_message => '%v%t' ],
     'Git::Push' => [],
-
     'MetaNoIndex' => [ map { (package => $_) } @no_index ],
+
 );
 
 my @config = (
